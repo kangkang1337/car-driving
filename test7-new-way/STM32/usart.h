6a70b22bc60efdef998edec84f9135135cebd9b5
kk <cutekk1337@gmail.com>
2026-09-04 14:30:28 +0800

    STM32 update

---
+++ test7-new-way/STM32/usart.h
@@ -0,0 +1,18 @@
+#ifndef __USART_H
+#define __USART_H
+
+#include "stdio.h"
+#include "sys.h"
+
+#define USART_REC_LEN 200
+#define EN_USART1_RX 1
+
+extern u8 USART_RX_BUF[USART_REC_LEN];
+extern u8 USART_RX_STA;
+extern u8 LED_CMD;
+extern u8 TURN_CMD;
+extern u8 END_CMD;
+
+void uart_init(u32 bound);
+
+#endif
